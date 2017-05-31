8d6a3bc185a61aa2e1ea650e2486f139c1c9c289
Dmitry Dev <dmitry.dev@gmail.com>
2017-05-31 11:10:57 +0300

    m5 homework ready

---
+++ src/m4hw/js/hw5.js
@@ -0,0 +1,85 @@
+/**
+ * Created by root on 31.05.2017.
+ */
+
+var callCount = 0;
+var sum = 0;
+
+function calcSum(a) {
+    if (!isNaN(parseFloat(a)) && isFinite(a)) {
+        var a = a;
+
+        if (callCount > 0) {
+            ++callCount;
+            sum += a;
+            return sum;
+        }
+        else {
+            ++callCount;
+            sum += a;
+            return a;
+        }
+    }
+    else {
+        ++callCount;
+        return 0;
+    }
+}
+
+
+console.log(calcSum(10));
+console.log(calcSum("sads"));
+console.log(calcSum(0));
+console.log(calcSum(10));
+console.log(calcSum(10));
+console.log("==============");
+
+function calcSumWithClosure() {
+    var callCount = 0;
+    var sum = 0;
+    return function (a) {
+        if (!isNaN(parseFloat(a)) && isFinite(a)) {
+            var a = a;
+
+            if (callCount > 0) {
+                ++callCount;
+                sum += a;
+                return sum;
+            }
+            else {
+                ++callCount;
+                sum += a;
+                return a;
+            }
+        }
+        else {
+            ++callCount;
+            return 0;
+        }
+    }
+}
+
+var f = calcSumWithClosure();
+
+console.log(f(10));
+console.log(f(10));
+console.log(f(0));
+console.log(f("asas"));
+console.log(f(25));
+console.log("==============");
+
+
+var f = function(obj){
+    obj.nw="new";
+}
+
+function getFunction(f){
+    var f = f;
+    var obj = {"k":"v","k1":1};
+    f(obj);
+    console.log(obj);
+}
+getFunction(f);
+
+
+
