0243301d2aba29f58be8d87cb06cec73db847c2c
dmitry.dev <dmitry.dev@gmail.com>
2017-06-15 23:17:13 +0300

    some changes...

--- src/nodestart/chains_hw/subobj.js
+++ src/nodestart/chains_hw/subobj.js
@@ -6,9 +6,16 @@ var User = {
     getCode: function (argument) {
         console.log(argument);
         return this;
+    },
+    addr: "O. Polya, 100",
+    Address: {
+        getAddress: function () {
+            //console.log(parent['addr']);
+            console.log('getAddress');
+            //return parent.this;
+        }
     }
 };
 //ответ на вчерашний вопрос по цепочкам - чтобы цепочки работали - метод объекта должен возвращать ссылку на самого себя..
-User.getName("name").getCode("code").getName("Name");
-
-
+User.getName("name").getCode("code").getName("Name")['Address'].getAddress();
+//['Address'].getAddress().getName('Name after addr')
