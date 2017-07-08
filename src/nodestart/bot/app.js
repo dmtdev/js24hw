d2d278a371b23a6c6baea8770d85a40c163db009
dmitry.dev <dmitry.dev@gmail.com>
2017-07-08 23:15:15 +0300

    dept..

---
+++ src/nodestart/bot/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var url = require('url');
+
+var app = express();
+
+
+
+app.get('/', function (req, res) {
+    var parsedURL = url.parse(req.url);
+    console.log();
+    res.send('get');
+});
+
+app.post('/', function (req, res) {
+    res.send('post');
+});
+
+app.listen(3000, function () {
+    console.log('start on 3000');
+});
