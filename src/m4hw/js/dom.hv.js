7ac4a0de796fc3522f4ec42cb1a5d10b439a5ae1
dmtdev <dmitry.dev@gmail.com>
2017-07-26 14:55:35 +0300

    Create dom.hv.js

---
+++ src/m4hw/js/dom.hv.js
@@ -0,0 +1,12 @@
+<script>
+document.querySelector('h2[name=Example]');
+document.getElementById('Example');
+document.getElementById('Notes');
+
+document.querySelector('span[name=goods_code]');
+document.querySelector('h1[class=detail-title]');
+document.getElementById('basic_image');
+document.getElementById('price_label');
+document.querySelector('ul[class=detail-short-details]');
+document.querySelector('div[class=detail-promo]');
+</script>
